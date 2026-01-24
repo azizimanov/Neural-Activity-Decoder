fe358ce9a603d48ee24037a9f54ce213bf0e9975
Aziz Imanov <aziz7imanov@gmail.com>
2026-01-23 19:49:55 -0800

    Add a DANDI downloader for NWB files

---
+++ src/project_brain_decoder/io/dandi_download.py
@@ -0,0 +1,36 @@
+import requests
+from pathlib import Path
+from nwb_loader import get_project_root
+
+
+SESSION_URLS = {
+    "session1": ("https://api.dandiarchive.org/api/assets/c002a9a1-664d-4a69-af02-ba810046c4fb/download/",
+                 "sub-Monkey-N_ses-20200127_ecephys.nwb"),
+    "session2": ("https://api.dandiarchive.org/api/assets/9d1820f1-7583-4faf-bbd0-7e9fb7001ca4/download/",
+                 "sub-Monkey-N_ses-20200130_ecephys.nwb"),
+    "session3": ("https://api.dandiarchive.org/api/assets/ea07a2e3-d5f4-4036-9b62-93d1f89cba64/download/",
+                 "sub-Monkey-N_ses-20200204_ecephys.nwb")
+}
+
+
+def download_session(url: tuple[str, str]):
+    """
+    Downloading the session dataset in chunks and saving it as an NWB file
+    :param url: tuple[str, str]
+    :return:
+    """
+    r = requests.get(url[0], stream=True)
+    r.raise_for_status()
+    root = get_project_root()
+    path = root / "data" / "raw" / url[1]
+    if not path.parent.exists():
+        # raise FileNotFoundError(f"Directory data/raw was not found: {path}")
+        path.parent.mkdir(parents=True, exist_ok=True)
+    with open(path.as_posix(), "wb") as f:
+        chunk_size = 4*1024*1024
+        for chunk in r.iter_content(chunk_size):
+            if chunk:
+                f.write(chunk)
+
+
+download_session(url=SESSION_URLS["session3"])
