165ae163266f3b16376c09d0b34ea9ad21589885
Aziz Imanov <aziz7imanov@gmail.com>
2026-02-01 12:26:11 -0800

    Safely close requests stream with context manager

--- src/project_brain_decoder/io/dandi_download.py
+++ src/project_brain_decoder/io/dandi_download.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from nwb_loader import get_project_root
 
 
+# Example data sessions from the DANDI archive:
 SESSION_URLS = {
     "session1": ("https://api.dandiarchive.org/api/assets/c002a9a1-664d-4a69-af02-ba810046c4fb/download/",
                  "sub-Monkey-N_ses-20200127_ecephys.nwb"),
@@ -19,18 +20,18 @@ def download_session(url: tuple[str, str]):
     :param url: tuple[str, str]
     :return:
     """
-    r = requests.get(url[0], stream=True)
-    r.raise_for_status()
-    root = get_project_root()
-    path = root / "data" / "raw" / url[1]
-    if not path.parent.exists():
-        # raise FileNotFoundError(f"Directory data/raw was not found: {path}")
-        path.parent.mkdir(parents=True, exist_ok=True)
-    with open(path.as_posix(), "wb") as f:
-        chunk_size = 4*1024*1024
-        for chunk in r.iter_content(chunk_size):
-            if chunk:
-                f.write(chunk)
+    with requests.get(url[0], stream=True) as r:
+        r.raise_for_status()
+        root = get_project_root()
+        path = root / "data" / "raw" / url[1]
+        if not path.parent.exists():
+            path.parent.mkdir(parents=True, exist_ok=True)
+        with open(path.as_posix(), "wb") as f:
+            chunk_size = 4*1024*1024
+            for chunk in r.iter_content(chunk_size):
+                if chunk:
+                    f.write(chunk)
+        return
 
 
 download_session(url=SESSION_URLS["session3"])
