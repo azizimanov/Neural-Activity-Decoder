cbd9eefa597060a08c8362148b7f85e5ed27a37c
Aziz Imanov <aziz7imanov@gmail.com>
2026-04-21 14:27:47 -0700

    add: initialize the GRU decoder class

--- src/project_brain_decoder/models/gru.py
+++ src/project_brain_decoder/models/gru.py
@@ -7,3 +7,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score
 from src.project_brain_decoder.io.nwb_loader import load_nwb
 
+
+class GRUDecoder:
+    """GRU-based neural decoder for 2D motor velocity prediction"""
+
+    def __init__(self, window_size, input_dim, batch_size, stride):
+        self.window_size = window_size
+        self.input_dim = input_dim
+        self.batch_size = batch_size
+        self.stride = stride
+        self.model = self._build_model()
+
