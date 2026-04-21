f8af82845a541706de6b7ebb4fc455df4cdb3a9d
Aziz Imanov <aziz7imanov@gmail.com>
2026-04-21 14:28:47 -0700

    add: gru model architecture

--- src/project_brain_decoder/models/gru.py
+++ src/project_brain_decoder/models/gru.py
@@ -18,3 +18,16 @@ class GRUDecoder:
         self.stride = stride
         self.model = self._build_model()
 
+
+    def _build_model(self):
+        """Builds a two-layer GRU followed by a Dense projection to 2D velocity"""
+        input_layer = Input(shape=(self.window_size, self.input_dim))
+        gru_1 = GRU(units=128, dropout=0.3, return_sequences=True)(input_layer)
+        gru_2 = GRU(units=64, dropout=0.5)(gru_1)
+        output = Dense(units=2)(gru_2)
+
+        model = Model(inputs=[input_layer], outputs=[output])
+        model.compile(optimizer=Adam(learning_rate=0.0005), loss="mse")
+        return model
+
+
