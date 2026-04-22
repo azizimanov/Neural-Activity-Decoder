c56ca4a78cfe3b8cd5cb412b033ef9ebb0d427e2
Aziz Imanov <aziz7imanov@gmail.com>
2026-04-21 18:52:27 -0700

    add: get R² and save them

--- src/project_brain_decoder/train_kalman.py
+++ src/project_brain_decoder/train_kalman.py
@@ -15,3 +15,13 @@ test = files[249:]
 # Train
 decoder = KalmanDecoder(n_components=50, dim_state=5)
 decoder.fit(train)
+
+# Evaluate on test sessions
+r2 = decoder.evaluate(test)
+print(f"Index vel. R²: {r2[0]:.4f}. MRS vel. R²: {r2[1]:.4f}")
+
+# Save scores
+save_r2.get_scores(model="kalman", score1=r2[0], score2=r2[1])
+
+
+
