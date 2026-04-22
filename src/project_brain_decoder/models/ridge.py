f22bde93a8c419d522242ab1cfa11cdeab19e7bb
Aziz Imanov <aziz7imanov@gmail.com>
2026-04-22 11:03:09 -0700

    migration: ridge decoder from Colab env

--- src/project_brain_decoder/models/ridge.py
+++ src/project_brain_decoder/models/ridge.py
@@ -1,4 +1,7 @@
+import numpy as np
+import pickle
 from sklearn.linear_model import Ridge
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import r2_score
+from src.project_brain_decoder.io.nwb_loader import load_nwb
 
-def get_ridge(alpha=1.0):
-    return Ridge(alpha=alpha)
