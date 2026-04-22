07880cb30ca78ec3a5b5691648baf6a851e6247e
Aziz Imanov <aziz7imanov@gmail.com>
2026-04-22 11:03:53 -0700

    add: ridge class initialization with hyperparameters

--- src/project_brain_decoder/models/ridge.py
+++ src/project_brain_decoder/models/ridge.py
@@ -5,3 +5,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import r2_score
 from src.project_brain_decoder.io.nwb_loader import load_nwb
 
+class RidgeDecoder:
+    """Ridge regression-based neural decoder for 2D motor velocity prediction"""
+
+    def __init__(self, alpha=1.0):
+        self.alpha = alpha
+        self.model = Ridge(alpha=alpha)
